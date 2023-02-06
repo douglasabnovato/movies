4845f1a2a3c4bf1da8d90fc84ac1510ec036f214
douglasabnovato <douglasabnovato@gmail.com>
2023-02-06 19:09:46 -0300

    o top da página details do filme clicado

--- src/pages/Details/index.js
+++ src/pages/Details/index.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
 import "./styles.css";
+import image from "../../assets/image26.png";
 
 function Details() {
   const API_KEY = "0e3950318bf412e11272f2f58c14e062";
@@ -16,6 +17,7 @@ function Details() {
     )
       .then((response) => response.json())
       .then((data) => {
+        console.log(data);
         const { title, poster_path, overview, release_date } = data;
         const movie = {
           id,
@@ -30,16 +32,117 @@ function Details() {
 
   return (
     <div className="main">
-      <p>id: {movie.id}</p>
-      <p>title: {movie.title}</p>
-      <p>sinopse: {movie.sinopse}</p>
-      <p>image: {movie.image}</p>
-      <p>releaseDate: {movie.releaseDate}</p>
-      <p>
-        <Link to="/">
-          <button>Go Back</button>
-        </Link>
-      </p>
+      
+      <div className="top">
+        
+        <div className="navbar">
+          <div className="title-site">
+            <div className="title-text">
+              TMDB
+              <span className="title-obj"></span>
+            </div>
+          </div>
+        </div>
+
+        <div className="film-detail">
+
+          <div className="film-banner">
+            <img src={image}></img>
+          </div>
+
+          <div className="film-infos">
+
+            <div className="info-title">
+              <p className="title-name">Deadpool (2016)</p>
+              <p className="title-head">
+                16 anos • 11/02/2016 (BR) • Ação, Aventura, Comédia, Ficção
+                científica • 1h 47m
+              </p>
+            </div>
+
+            <div className="info-evaluation">
+              <p className="evaluation-loading">76%</p>
+              <p className="evaluation-evaluation">Avaliação dos usuários</p>
+            </div>
+            <div className="info-sinopse">
+              <p className="sinopse-title">Sinopse</p>
+              <p className="sinopse-text">
+                Baseado no anti-herói não convencional da Marvel Comics,
+                Deadpool conta a história da origem do ex-agente das Forças
+                Especiais que se tornou o mercenário Wade Wilson. Depois de ser
+                submetido a um desonesto experimento que o deixa com poderes de
+                cura acelerada, Wade adota o alter ego de Deadpool. Armado com
+                suas novas habilidades e um senso de humor negro e distorcido,
+                Deadpool persegue o homem que quase destruiu sua vida.
+              </p>
+            </div>
+            <div className="info-datasheet">
+              <div className="datasheet-1">
+                <p className="datasheet-title">Rob Liefeld</p>
+                <p className="datasheet-text">Characters</p>
+              </div>
+              <div className="datasheet-1">
+                <p className="datasheet-title">Rhett Reese</p>
+                <p className="datasheet-text">Screenplay</p>
+              </div>
+              <div className="datasheet-1">
+                <p className="datasheet-title">Fabian Nicieza</p>
+                <p className="datasheet-text">Characters</p>
+              </div>
+              <div className="datasheet-1">
+                <p className="datasheet-title">Paul Wernick</p>
+                <p className="datasheet-text">Screenplay</p>
+              </div>
+              <div className="datasheet-1">
+                <p className="datasheet-title">Tim Miller</p>
+                <p className="datasheet-text">Director</p>
+              </div>
+            </div>
+          </div>
+        </div>
+        
+      </div>
+
+      <div className="cast">
+        <p>id: {movie.id}</p>
+        <p>title: {movie.title}</p>
+        <p>sinopse: {movie.sinopse}</p>
+        <p>image: {movie.image}</p>
+        <p>releaseDate: {movie.releaseDate}</p>
+      </div>
+
+      <div className="bar">
+        <p> vvvvv </p>
+      </div>
+
+      <div className="trailer">
+        <p>id: {movie.id}</p>
+        <p>title: {movie.title}</p>
+        <p>sinopse: {movie.sinopse}</p>
+        <p>image: {movie.image}</p>
+        <p>releaseDate: {movie.releaseDate}</p>
+      </div>
+
+      <div className="recommendations">
+        <p>id: {movie.id}</p>
+        <p>title: {movie.title}</p>
+        <p>sinopse: {movie.sinopse}</p>
+        <p>image: {movie.image}</p>
+        <p>releaseDate: {movie.releaseDate}</p>
+      </div>
+
+      <div className="pagination">
+        <div className="contagem">
+          <span className="numeros"> 1 2 3 4 5 </span>
+          <span className="seta"> &gt; </span>
+          <span className="posicao">Última</span>
+        </div>
+        <p>
+          <Link to="/">
+            <button>Go Back</button>
+          </Link>
+        </p>
+      </div>
     </div>
   );
 }
